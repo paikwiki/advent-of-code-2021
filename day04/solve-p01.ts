6f04e8a2fe81e36b31b34ea36f24619fe34aa676
paikwiki <paikwiki@gmail.com>
2021-12-11 17:48:58 +0900

    :sparkles: Solve Day04-01

--- day04/solve-p01.ts
+++ day04/solve-p01.ts
@@ -26,13 +26,10 @@ const run = async () => {
     bingoBoardTemplate[rawIndex] = initBingoRaw();
   }
 
-  console.log("bingoBoardCount", bingoBoardCount);
-
-  const bingoBoards: BingoBoard[] = [];
+  let bingoBoards: BingoBoard[] = [];
   for (let idx = 0; idx < bingoBoardCount; idx++) {
     bingoBoards.push(JSON.parse(JSON.stringify(bingoBoardTemplate)));
   }
-  console.log(bingoBoards);
 
   // set bingoBoards
   for (let idx = 0; idx < linesForBingoBoard.length; idx++) {
@@ -43,7 +40,79 @@ const run = async () => {
       .filter((value) => value.length !== 0)
       .map((value) => ({ value, marked: false }));
   }
-  return bingoBoards;
+
+  // check
+  for (const calledNumber of instruction.split(",")) {
+    bingoBoards = bingoBoards.map((bingoBoard) =>
+      bingoBoard.map((bingoRaw) =>
+        bingoRaw.map((item) =>
+          item.value === calledNumber ? { ...item, marked: true } : item
+        )
+      )
+    );
+
+    let isBingo = false;
+    let winner = -1;
+
+    // check each raw
+    bingoBoards.map((bingoBoard, index) =>
+      bingoBoard.map((bingoRaw) => {
+        const trueItems = bingoRaw.filter((item) => item.marked);
+        if (trueItems.length === 5) {
+          isBingo = true;
+          winner = index;
+        }
+      })
+    );
+
+    // check each column
+    !isBingo &&
+      bingoBoards.map((bingoBoard, index) => {
+        const columnTrueItems = [0, 1, 2, 3, 4].map((columnNumber) =>
+          bingoBoard.filter((bingoRaw) => bingoRaw[columnNumber].marked)
+        );
+        columnTrueItems.map((col) => {
+          if (col.length === 5) {
+            isBingo = true;
+            winner = index;
+          }
+        });
+      });
+
+    // check diagonal
+    !isBingo &&
+      bingoBoards.map((bingoBoard, index) => {
+        if (
+          (bingoBoard[0][0].marked &&
+            bingoBoard[1][1].marked &&
+            bingoBoard[2][2].marked &&
+            bingoBoard[3][3].marked &&
+            bingoBoard[4][4].marked) ||
+          (bingoBoard[0][4].marked &&
+            bingoBoard[1][3].marked &&
+            bingoBoard[2][2].marked &&
+            bingoBoard[3][1].marked &&
+            bingoBoard[4][0].marked)
+        ) {
+          isBingo = true;
+          winner = index;
+        }
+      });
+
+    if (isBingo)
+      return (
+        parseInt(calledNumber) *
+        bingoBoards[winner].reduce(
+          (acc, bingoRaw) =>
+            (acc += bingoRaw.reduce(
+              (rawAcc, item) =>
+                item.marked ? rawAcc : (rawAcc += parseInt(item.value)),
+              0
+            )),
+          0
+        )
+      );
+  }
 };
 
 run().then((res) => console.log(JSON.stringify(res, null, 2)));
